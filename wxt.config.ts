90bb276622f7c50831f07c6494967d5e0723a796
skidoodle <contact@albert.lol>
2025-03-19 22:08:20 +0100

    fix permissions (remove webrequest)

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'wxt'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  extensionApi: 'webextension-polyfill',
   modules: ['@wxt-dev/module-react'],
   manifest: {
-    permissions: ['tabs', 'activeTab', 'webRequest'],
-    host_permissions: ['https://ip.albert.lol/*', 'https://cloudflare-dns.com/*'],
+    permissions: ['tabs', 'activeTab'],
+    host_permissions: [
+      'https://ip.albert.lol/*',
+      'https://cloudflare-dns.com/*',
+    ],
     action: {
       default_title: 'Host Info',
     },
