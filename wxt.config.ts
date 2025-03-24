6bf00dc6e02504a90c08f4b00221fa7bfd726603
skidoodle <contact@albert.lol>
2025-03-24 20:26:11 +0100

    The extension ID is required in Manifest Version 3 and above.

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,7 +3,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  manifestVersion: 3,
   manifest: {
+    name: 'Host Info',
+    description: 'Get host information',
+    version: '1.7',
+    author: { email: 'contact@albert.lol' },
     permissions: ['tabs', 'activeTab'],
     host_permissions: [
       'https://ip.albert.lol/*',
@@ -12,6 +17,11 @@ export default defineConfig({
     action: {
       default_title: 'Host Info',
     },
+    browser_specific_settings: {
+      gecko: {
+        id: '{6271af71-3c09-452a-9c28-181723a4ed96}',
+      },
+    }
   },
   vite: () => ({
     plugins: [tailwindcss()],
