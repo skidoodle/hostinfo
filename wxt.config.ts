fea3cf5a6285d3d67db1a070f9e439f9848b4d54
skidoodle <contact@albert.lol>
2026-02-24 16:49:50 +0100

    Release 2.1

--- wxt.config.ts
+++ wxt.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   manifest: {
     name: 'Host Info',
     description: 'Get host information',
-    version: '2.0.0',
+    version: '2.1',
     permissions: [
       'tabs',
       'webRequest',
