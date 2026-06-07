8a7b1a60642a6e7c8d52d56c7d5e748243c237d9
skidoodle <contact@albert.lol>
2026-06-08 01:36:17 +0200

    i hate firefox

--- wxt.config.ts
+++ wxt.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
     },
     browser_specific_settings: {
       gecko: {
+        id: 'hostinfo@albert.lol',
         strict_min_version: '142.0',
         data_collection_permissions: {
           required: ['none']
